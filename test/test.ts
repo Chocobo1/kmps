7cd81499f2429bdd28a5ea68d6c1a2227c604c11
Chocobo1 <Chocobo1@users.noreply.github.com>
2022-12-11 21:32:32 +0800

    Add testing for Readme example

--- test/test.ts
+++ test/test.ts
@@ -175,3 +175,42 @@ describe("Property tests", function () {
         });
     });
 });
+
+describe("Example tests", function () {
+    it("Readme examples", function() {
+        // working with TypedArray
+        {
+            const pattern = Uint32Array.from([0xFFFF, 0x3000]);
+            const corpus = Uint32Array.from([0xFFFF, 0xFFFF, 0x3000, 0x1000]);
+
+            // setup `kmp` for later reuse
+            const kmp = Kmp.KnuthMorrisPratt(pattern);
+            // returns the first index of the exact match in `corpus`; -1 if not found
+            const idx = kmp.match(corpus);
+            if (idx !== 1)
+                throw (new Error('Please file an issue'));
+        }
+
+        // also working with String
+        {
+            const pattern = "pattern";
+            const corpus = "some pattern !@#$%";
+
+            const kmp = Kmp.KnuthMorrisPratt(pattern);
+            const idx = kmp.match(corpus);
+            if (idx !== corpus.indexOf(pattern))
+                throw (new Error('Please file an issue'));
+        }
+
+        // you can specify offset!
+        {
+            const pattern = "123";
+            const corpus = "123abc123";
+            const corpusOffset = 1;
+
+            const idx = Kmp.KnuthMorrisPratt(pattern).match(corpus, corpusOffset);
+            if (idx !== corpus.indexOf(pattern, corpusOffset))
+                throw (new Error('Please file an issue'));
+        }
+    });
+});
